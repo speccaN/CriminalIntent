b2afc6c40aee28b0368e936be4bc36469dad16bc
speccaN <ericlindeberg@hotmail.com>
2016-02-22 22:35:53 -0600

    close #2

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,26 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.UUID;
+
+public class Crime {
+
+    private UUID mID;
+    private String mTitle;
+
+    public Crime() {
+        // Generate unique identifier
+        mID = UUID.randomUUID();
+    }
+
+    public UUID getID() {
+        return mID;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
